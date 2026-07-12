5ef59e96d6181dd0b88e921c9445092b51793d82
anshbhatt <anshbhatt944@gmail.com>
2026-07-13 03:17:05 +0530

    Fix output formatting in printArray function of Selection Sort

--- SortingAlgorithms/SelectionSort.cpp
+++ SortingAlgorithms/SelectionSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void printArray(int arr[], int size){
     for(int i=0;i<size;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
 }
 void selectionSort(int arr[], int size){

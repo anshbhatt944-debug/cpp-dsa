f0f9dd6e9c8240db090f6868869e846e5abbe530
anshbhatt <anshbhatt944@gmail.com>
2026-07-13 18:39:49 +0530

    Refactor insertionSort function and update main to use it

--- SortingAlgorithms/practice.cpp
+++ SortingAlgorithms/practice.cpp
@@ -16,9 +16,25 @@ void selectionSort(int arr[], int size){
         swap(arr[minIndex], arr[i]);
     }
 }
+void insertionSort(int arr[], int size){
+    for(int i=1;i<size;i++){
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0){
+            if(arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }else{
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
 int main(){
     int arr[]={64, 25, 12, 22, 11};
-    selectionSort(arr, 4);
+    //selectionSort(arr, 4);
+    insertionSort(arr, 4);
     printArray(arr,4);
     return 0;
 }

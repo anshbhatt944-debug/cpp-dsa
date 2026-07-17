e6efc4c8b36a635192c729c766e86e0baf9b24a6
anshbhatt <anshbhatt944@gmail.com>
2026-07-18 04:49:06 +0530

    Refactor STL_Algorithms.cpp: improve output formatting and correct lower bound example

--- STL (Standard Template Library)/STL_Algorithms.cpp
+++ STL (Standard Template Library)/STL_Algorithms.cpp
@@ -4,14 +4,25 @@
 using namespace std;
 int main(){
     vector<int> v;
-
     v.push_back(1);
     v.push_back(3);
     v.push_back(6);
     v.push_back(7);
-
-
     cout<<"Finding 6-> "<<binary_search(v.begin(),v.end(),6)<<endl;
-    cout<<"lower bound--> "<<lower_bound(v.begin(),v.end(),2)-v.begin()<<endl;
+    cout<<"Lower Bound--> "<<lower_bound(v.begin(),v.end(),6)-v.begin()<<endl;
+    cout<<"Upper Bound--> "<<upper_bound(v.begin(),v.end(),4)-v.begin()<<endl;
+    int a=3;
+    int b=5;
+    cout<<"max -> "<<max(a,b)<<endl;
+    cout<<"min -> "<<min(a,b)<<endl;
+    swap(a,b);
+    string abcd = "abcd";
+    reverse(abcd.begin(),abcd.end());
+    cout<<"string-> "<<abcd<<endl;
+    rotate(v.begin(),v.begin()+1,v.end());
+    cout<<"after rotate"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }
     return 0;
 }
